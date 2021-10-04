f2b4558a910d0e5072470f5d7e2c834ef31870ee
kkdshka <kkdshka@gmail.com>
2021-10-04 13:32:59 +0300

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopSection } from "./components/topSection/TopSection";
+
+export const App = () => {
+  return (
+    <div className={"App"}>
+      <TopSection />
+    </div>
+  );
+};
---
+++ src/components/topSection/TopSection.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./TopSection.scss";
+
+export const TopSection = () => {
+  return <div className={"container"}>Hello world</div>;
+};
