6e68d60d4ddfc5509232a371aec3f0bc4c33853c
kkdshka <kkdshka@gmail.com>
2021-10-13 14:57:58 +0300

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { TopSection } from "./components/topSection/TopSection";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/home/Home";
 
 export const App = () => {
   return (
     <div className="App">
-      <TopSection />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { TopSection } from "../topSection/TopSection";
+
+export const Home = () => {
+  return <TopSection />;
+};
