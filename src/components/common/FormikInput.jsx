7ecc550eb8cf38a34cf6e8a32fd026905723c8ee
kkdshka <kkdshka@gmail.com>
2021-10-19 12:35:30 +0300

    Added colors to input borders

--- src/components/common/FormikInput.jsx
+++ src/components/common/FormikInput.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import "./FormikInput.scss";
 
 export const FormikInput = ({ id, formik, type = "text", ...props }) => {
+  const withError = formik.touched[id] && formik.errors[id];
+
+  const inputClassName = withError
+    ? "formik-input input-error"
+    : "formik-input";
+
   return (
     <div className="formik-input_container">
       <input
         id={id}
         type={type}
-        className="formik-input"
+        className={inputClassName}
         {...formik.getFieldProps(id)}
         {...props}
       />
-      {formik.touched[id] && formik.errors[id] ? (
+      {withError ? (
         <div className="formik-input_error-message">{formik.errors[id]}</div>
       ) : null}
     </div>
