99b9de47046ee5e05814fb42d9d116ed17beeeb0
kkdshka <kkdshka@gmail.com>
2021-10-20 13:23:45 +0300

    Added classnames

--- src/components/common/FormikInput.jsx
+++ src/components/common/FormikInput.jsx
@@ -1,24 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 import "./FormikInput.scss";
 
 export const FormikInput = ({ id, formik, type = "text", ...props }) => {
   const withError = formik.touched[id] && formik.errors[id];
-
-  const inputClassName = withError
-    ? "formik-input input-error"
-    : "formik-input";
 
   return (
     <div className="formik-input_container">
       <input
         id={id}
         type={type}
-        className={inputClassName}
+        className={classNames("formik-input", { "input-error": withError })}
         {...formik.getFieldProps(id)}
         {...props}
       />
       {withError ? (
-        <div className="formik-input_error-message">{formik.errors[id]}</div>
+        <div className={"formik-input_error-message"}>{formik.errors[id]}</div>
       ) : null}
     </div>
   );
