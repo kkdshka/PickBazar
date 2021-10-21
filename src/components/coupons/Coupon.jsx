f3dea991dd563c670702750101407b8f383eb555
kkdshka <kkdshka@gmail.com>
2021-10-21 13:19:12 +0300

    Added markup for coupon

--- src/components/coupons/Coupon.jsx
+++ src/components/coupons/Coupon.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./Coupon.scss";
 
-export const Coupon = ({ coupon }) => {
-  return <div className="coupon" />;
+export const Coupon = ({
+  coupon: { title, description, buttonText, gradientColors },
+}) => {
+  return (
+    <div
+      className="coupon"
+      style={{
+        background: `linear-gradient(90deg, ${gradientColors.start} 0%, ${gradientColors.end} 100%)`,
+      }}
+    >
+      <div className="coupon_title">{title}</div>
+      <div className="coupon_description">{description}</div>
+      <button
+        className="coupon_button-text"
+        style={{ color: gradientColors.start }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
 };
--- src/components/coupons/Coupons.jsx
+++ src/components/coupons/Coupons.jsx
@@ -7,21 +7,9 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 4000, min: 0 },
     items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
   },
 };
 
