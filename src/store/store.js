c1f1cf9c77e87e329fcba25cfd582ad077705800
kkdshka <kkdshka@gmail.com>
2021-11-03 14:53:51 +0200

    Added cart store saving.

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import signUpReducer from "./signUpSlice";
 import authReducer from "./authSlice";
 import signInReducer from "./signInSlice";
@@ -8,15 +11,24 @@ import productsReducer from "./productsSlice";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 
+const reducers = combineReducers({
+  signUp: signUpReducer,
+  signIn: signInReducer,
+  auth: authReducer,
+  coupons: couponsReducer,
+  categories: categoriesReducer,
+  products: productsReducer,
+  product: productReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: "cart",
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    signUp: signUpReducer,
-    signIn: signInReducer,
-    auth: authReducer,
-    coupons: couponsReducer,
-    categories: categoriesReducer,
-    products: productsReducer,
-    product: productReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
 });
