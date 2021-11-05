c01754776fc2c8eb4d34ef7d4498aa65a751a935
kkdshka <kkdshka@gmail.com>
2021-11-05 13:37:35 +0200

    Fixed Non-Serializable Data error.

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import signUpReducer from "./signUpSlice";
 import authReducer from "./authSlice";
 import signInReducer from "./signInSlice";
@@ -32,4 +40,10 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
